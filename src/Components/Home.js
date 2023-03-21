4950bf444884bcc504200754e4b3c6b134a8af3f
atulShinde <atul.mobicloud@gamil.com>
2023-03-21 15:47:42 +0530

    NA

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,6 +7,9 @@ const Home = () => {
  
   return (
     <div className="container">
+      <span style={{'color': 'red', 'fontSize': '30px', 'fontWeight': 'bolder'}}>AS</span>
+      <span style={{'color': 'Green', 'fontSize': '20px', 'fontWeight': 'bolder', 'fontStyle': 'oblique'}}>MOVIES</span>
+
         <Search />
         <Movie />
     </div>
